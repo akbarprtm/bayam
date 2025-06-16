04dade36b7be49753e9b50e19f3bd8835bc2f1e2
akbarprtm <129855516+akbarprtm@users.noreply.github.com>
2025-06-16 11:53:26 +0700

    Update save_penyiraman.js

--- api/save_penyiraman.js
+++ api/save_penyiraman.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
 
   const supabaseUrl = 'https://ctggbrmvubjggyxmmbse.supabase.co';
   const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0Z2dicm12dWJqZ2d5eG1tYnNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyNTE4NDcsImV4cCI6MjA2MzgyNzg0N30.6la5T8_8wrme55wKM7_r7kA6SO90-ht8JlP8aE3C6UA';
-  const table = "penyiraman"; // ganti langsung jadi "sensor_data"
+  const table = 'penyiraman';
 
   try {
     const response = await fetch(`${supabaseUrl}/rest/v1/${table}`, {
@@ -19,9 +19,9 @@ export default async function handler(req, res) {
         'Prefer': 'return=representation'
       },
       body: JSON.stringify({
-        metode,
-        durasi: parseInt(durasi),
-        waktu
+        metode: metode,
+        durasi_detik: parseInt(durasi),
+        waktu: waktu
       })
     });
 
@@ -31,9 +31,9 @@ export default async function handler(req, res) {
       return res.status(500).json({ error: 'Gagal menyimpan ke Supabase', detail: data });
     }
 
-    return res.status(200).json({ message: 'Penyiraman berhasil disimpan', data });
+    return res.status(200).json({ message: '✅ Penyiraman berhasil disimpan', data });
 
   } catch (err) {
-    return res.status(500).json({ error: 'Terjadi kesalahan server', detail: err.message });
+    return res.status(500).json({ error: '❌ Terjadi kesalahan server', detail: err.message });
   }
 }
