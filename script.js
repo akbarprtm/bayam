9b7111fd47dc0815a2f3fb1560831191b9586b23
akbarprtm <129855516+akbarprtm@users.noreply.github.com>
2025-07-27 17:29:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<script type="module">
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 const supabase = createClient(
   'https://ctggbrmvubjggyxmmbse.supabase.co',
