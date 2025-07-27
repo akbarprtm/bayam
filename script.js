01ad6bac2221f943dbcfee553120b6c58840e9ce
akbarprtm <129855516+akbarprtm@users.noreply.github.com>
2025-07-27 13:59:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Ganti dengan kredensial Supabase kamu (hindari expose PUBLIC KEY di frontend production)
 const SUPABASE_URL = 'https://ctggbrmvubjggyxmmbse.supabase.co';
-const SUPABASE_KEY = 'PUBLIC_ANON_KEY_HANYA_BOLEH_DIGUNAKAN_JIKA_TIDAK_SENSITIF';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0Z2dicm12dWJqZ2d5eG1tYnNlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0ODI1MTg0NywiZXhwIjoyMDYzODI3ODQ3fQ.6rVGqPTOCkhI14R12cRVSQfH0uF7ywzQIC7Dm-vSrZA';
 
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
